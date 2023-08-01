4d221f8c835318c3411ed87a1c035bca5e8fabfc
Isaac Kabucho <kabszac@gmail.com>
2023-08-01 21:27:37 +0300

    learnt about flex

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,19 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="bg-red-400 border-4 border-green-500">
-        <h1 className="text-wanja"> Hello World</h1>
+    <main className="">
+      <div className="bg-red-400 border-4 border-green-500 p-3 m-2">
+        <h1 className="text-wanja text-4xl"> Hello World</h1>
+      </div>
+      <div className="flex flex-row-reverse hover:flex-col md:flex-row ">
+        <div className="basis-1/4 bg-rose-500 text-white text-center px-8 py-4 border-2 rounded">
+          01
+        </div>
+        <div className="basis-1/4 hover:basis-1/2  bg-rose-500 text-white text-center px-8 py-4 border-2 rounded">
+          02
+        </div>
+        <div className="basis-1/2  bg-rose-500 text-white text-center px-8 py-4 border-2 rounded">
+          03
+        </div>
       </div>
     </main>
   );
