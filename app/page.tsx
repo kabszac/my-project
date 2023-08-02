10280e6c9f585e88df998d7caf307651bf16af4b
Isaac Kabucho <kabszac@gmail.com>
2023-08-02 15:22:15 +0300

    learnt about grid

--- app/page.tsx
+++ app/page.tsx
@@ -15,6 +15,29 @@ export default function Home() {
           03
         </div>
       </div>
+      <div className="grid grid-cols-3 gap-4">
+        <div className=" bg-rose-500 text-white text-center px-8 py-4 border-2 rounded">
+          01
+        </div>
+        <div className=" bg-rose-500 text-white text-center px-8 py-4 border-2 rounded">
+          02
+        </div>
+        <div className=" bg-rose-500 text-white text-center px-8 py-4 border-2 rounded">
+          03
+        </div>
+        <div className="col-span-2 row-span-2  bg-rose-500 text-white text-center px-8 py-4 border-2 rounded">
+          04
+        </div>
+        <div className=" bg-rose-500 text-white text-center px-8 py-4 border-2 rounded">
+          05
+        </div>
+        <div className="col-start-1 col-end-5  bg-rose-500 text-white text-center px-8 py-4 border-2 rounded">
+          06
+        </div>
+        <div className="col-span-2  bg-rose-500 text-white text-center px-8 py-4 border-2 rounded">
+          07
+        </div>
+      </div>
     </main>
   );
 }
